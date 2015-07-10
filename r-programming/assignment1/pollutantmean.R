ddcf5fd7e01131e05344580f95a6eca4d29a1f58
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2015-07-11 00:57:50 +0500

    pollutantmean.R finished and submitted!

--- r-programming/assignment1/pollutantmean.R
+++ r-programming/assignment1/pollutantmean.R
@@ -1,7 +1,16 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
-  
+  mean.data <- c()
+  files <- list.files(directory)
+  old.wd <- getwd()
+  setwd(directory)
+  for(num in id) {
+    file.data <- read.csv(files[num])
+    pollutant.vector <- file.data[[pollutant]]
+    mean.data <- c(mean.data, pollutant.vector)
+  }
+  setwd(old.wd)
   ## 'pollutant' is a character vector of length 1 indicating
   ## the name of the pollutant for which we will calculate the
   ## mean; either "sulfate" or "nitrate".
@@ -12,4 +21,5 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
   ## NOTE: Do not round the result!
+  mean(mean.data, na.rm = TRUE)
 }
