8cd69578df9b45e0b73e97ded4d9d2254ecd1f71
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2015-07-11 20:26:46 +0500

    Added the final corr function. And submitted.

--- r-programming/assignment1/corr.R
+++ r-programming/assignment1/corr.R
@@ -1,6 +1,19 @@
 corr <- function(directory, threshold = 0) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
+  old.wd <- getwd()
+  files <- list.files(directory)
+  corrs <- c()
+  obs <- complete(directory)
+  sel <- subset(obs, nobs > threshold)
+  setwd(directory)
+  
+  for(i in sel$id) {
+    dat <- read.csv(files[i])
+    nitr <- dat$nitrate
+    sulf <- dat$sulfate
+    corrs <- c(corrs, cor(sulf, nitr, use = "pairwise.complete.obs"))
+  }
   
   ## 'threshold' is a numeric vector of length 1 indicating the
   ## number of completely observed observations (on all
@@ -9,4 +22,6 @@ corr <- function(directory, threshold = 0) {
   
   ## Return a numeric vector of correlations
   ## NOTE: Do not round the result!
+  setwd(old.wd)
+  corrs
 }
