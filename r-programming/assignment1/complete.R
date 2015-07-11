bcc69f9dd8442ae6551c94a2525e2ef9e70da9f5
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2015-07-11 19:29:04 +0500

    Cleaning up and making code readable.

--- r-programming/assignment1/complete.R
+++ r-programming/assignment1/complete.R
@@ -1,7 +1,16 @@
 complete <- function(directory, id = 1:332) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
-  
+  old.wd <- getwd()
+  files <- list.files(directory)
+  setwd(directory)
+  nobs <- numeric()
+  for(num in id) {
+    dat <- read.csv(files[num])
+    l <- dim(dat)[1]
+    nas <- sum(is.na((dat$sulfate & dat$nitrate)))
+    nobs <- c(nobs, l - nas)
+  }
   ## 'id' is an integer vector indicating the monitor ID numbers
   ## to be used
   
@@ -12,4 +21,6 @@ complete <- function(directory, id = 1:332) {
   ## ...
   ## where 'id' is the monitor ID number and 'nobs' is the
   ## number of complete cases
+  setwd(old.wd)
+  data.frame(cbind(id, nobs))
 }
